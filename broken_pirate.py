14e4e96f15fcf088dc89ef3f6e57ea59a80248dc
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-24 12:33:34 +0400

    Added the closing double quotes on line 1

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,4 +1,4 @@
-greeting = input("Hello, possible pirate! What's the password?)
+greeting = input("Hello, possible pirate! What's the password?") #added the closing double quotes
 if greeting in ["Arrr!"]:   #corrected the parantheses to square brackets
     print("Go away, pirate.") #indentation
 else :  #added colon and changed the condition to else
