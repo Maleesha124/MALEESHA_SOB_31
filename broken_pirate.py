f3ad1ac0af74f0ae6e435ba7c1bc4c5d5da5ab47
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-24 12:31:18 +0400

    corrected the indentation on line 5

--- broken_pirate.py
+++ broken_pirate.py
@@ -2,4 +2,4 @@ greeting = input("Hello, possible pirate! What's the password?)
 if greeting in ["Arrr!"]:   #corrected the parantheses to square brackets
 	print("Go away, pirate.")
 else :  #added colon and changed the condition to else
-print("Greetings, hater of pirates!")
+    print("Greetings, hater of pirates!") #indentation 
