a9d91f944362f182a9fb65c9623aa19347e1fecd
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-24 12:27:43 +0400

    corrected the parantheses to square brackets on line 2

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
 greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+if greeting in ["Arrr!"]:   #corrected the parantheses to square brackets
 	print("Go away, pirate.")
 elif
 print("Greetings, hater of pirates!")
