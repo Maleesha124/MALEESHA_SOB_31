9eeddd12b3949c17491286aa92ade65385657389
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-24 12:08:27 +0400

    corrected variable name on line 30

--- broken_grades.py
+++ broken_grades.py
@@ -27,7 +27,7 @@ exam_3 = int(input("Input exam grade three: ")) #change str to int
 
 grades = [exam_one, exam_two, exam_three] #added missing commas
 sum = 0
-for grade in grade:
+for grade in grades: #corrected the name of the variable
   sum = sum + grade
 
 avg = sum / len(grdes)
