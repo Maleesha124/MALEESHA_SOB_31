8f917190e156001baff01c2f579d00a8e44bd058
Maleesha Chamodi <maleeshachamodi05@gmail.com>
2023-11-24 12:29:57 +0400

    Added colon and changed the condition to else

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
 greeting = input("Hello, possible pirate! What's the password?)
 if greeting in ["Arrr!"]:   #corrected the parantheses to square brackets
 	print("Go away, pirate.")
-elif
+else :  #added colon and changed the condition to else
 print("Greetings, hater of pirates!")
